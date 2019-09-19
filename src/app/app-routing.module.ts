75078bbf57bed238ef93729a6c347af916a7b2a4
ChouaibBELILITA <ec_belilita@esi.dz>
2019-09-19 03:13:35 +0100

    page-apropos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AceuilComponent } from './components/aceuil/aceuil.component'
 import { ApaComponent } from 'src/app/components/apa/apa.component';
+import { aproposComponent } from 'src/app/components/apropos/apropos.component';
+
 import { GedComponent } from 'src/app/components/ged/ged.component';
 import { RessourceComponent } from 'src/app/components/ressource/ressource.component';
 import { AnnuaireComponent } from 'src/app/components/annuaire/annuaire.component';
@@ -16,6 +18,7 @@ import { AnnuairePagesComponent } from './components/annuaire/annuairePages/annu
 const routes: Routes = [
   { path:'',component:AceuilComponent },
   { path:'apa',component:ApaComponent},
+  { path:'apropos',component:aproposComponent},
   { path:'ged',component:GedComponent},
   { path:'RG',component:AnnuaireComponent },
   { path:'Ressource',component:RessourceComponent},
---
+++ src/app/components/apropos/apropos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ressource',
+  templateUrl: './apropos.component.html',
+  styleUrls: ['./apropos.component.css']
+})
+export class aproposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
